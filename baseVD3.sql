c3bf7f677086f19013e12a276c2da90b7ac9ab6b
mikeed1998 <mikeed1998@gmail.com>
2022-08-19 17:58:01 -0500

    cambios tercer archivo

--- baseVD3.sql
+++ baseVD3.sql
@@ -1,2 +1,35 @@
 CREATE DATABASE Ejercicio;
 
+CREATE TABLE Empresa(
+	id              INT       PRIMARY KEY NOT NULL,
+	id_actividad    INT       NOT NULL
+);
+
+CREATE TABLE Actividad(
+	id             INT             PRIMARY KEY NOT NULL,
+	nombre         VARCHAR(50)     NOT NULL,
+	estatus        BOOLEAN         NOT NULL,
+	id_usuario     INT             NOT NULL
+);
+
+ALTER TABLE Empresa
+ADD CONSTRAINT FK_empresaActividad FOREIGN KEY (id_actividad) REFERENCES Actividad(id);
+
+CREATE TABLE Usuario(
+	id         INT           PRIMARY KEY NOT NULL,
+	nombre     VARCHAR(50)   NOT NULL,
+	id_tarea   INT           NOT NULL
+);
+
+CREATE TABLE Tarea(
+	nombre               VARCHAR(50)      NOT NULL,
+	descripcion          VARCHAR(200)     NOT NULL,
+	nombre_usuario       VARCHAR(50)      NOT NULL,
+	fecha_inicio         DATE             NOT NULL,
+	fecha_vencimiento    DATE             NOT NULL,
+	id_usuario           INT              NOT NULL,
+	CONSTRAINT FK_tareaUsuario FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
+);
+
+
+
