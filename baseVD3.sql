ec95de53d1afcf2faedf83c86869ac172f152e03
mikeed1998 <mikeed1998@gmail.com>
2022-08-22 01:05:37 -0500

    cambios

--- baseVD3.sql
+++ baseVD3.sql
@@ -1,35 +1,53 @@
 CREATE DATABASE Ejercicio;
 
+CREATE TABLE Tarea(
+	id                   INT              PRIMARY KEY NOT NULL,
+	nombre               VARCHAR(50)      NOT NULL,
+	descripcion          VARCHAR(200)     NOT NULL,
+	nombre_usuario       VARCHAR(50)      NOT NULL,
+	fecha_inicio         VARCHAR(10)      NOT NULL,
+	fecha_vencimiento    VARCHAR(10)      NOT NULL,
+	estatus        		   BOOLEAN          NOT NULL
+);
+
 CREATE TABLE Empresa(
-	id              INT       PRIMARY KEY NOT NULL,
-	id_actividad    INT       NOT NULL
+	id            INT            PRIMARY KEY NOT NULL,
+	nombre        VARCHAR(50)    NOT NULL,
+	id_tarea      INT            NOT NULL,
+	CONSTRAINT FK_empresaTarea FOREIGN KEY (id_tarea) REFERENCES Tarea(id)
 );
-
-CREATE TABLE Actividad(
-	id             INT             PRIMARY KEY NOT NULL,
-	nombre         VARCHAR(50)     NOT NULL,
-	estatus        BOOLEAN         NOT NULL,
-	id_usuario     INT             NOT NULL
-);
-
-ALTER TABLE Empresa
-ADD CONSTRAINT FK_empresaActividad FOREIGN KEY (id_actividad) REFERENCES Actividad(id);
 
 CREATE TABLE Usuario(
 	id         INT           PRIMARY KEY NOT NULL,
 	nombre     VARCHAR(50)   NOT NULL,
-	id_tarea   INT           NOT NULL
+	id_tarea   INT           NOT NULL,
+	CONSTRAINT FK_usuarioTarea FOREIGN KEY (id_tarea) REFERENCES Tarea(id)
 );
 
-CREATE TABLE Tarea(
-	nombre               VARCHAR(50)      NOT NULL,
-	descripcion          VARCHAR(200)     NOT NULL,
-	nombre_usuario       VARCHAR(50)      NOT NULL,
-	fecha_inicio         DATE             NOT NULL,
-	fecha_vencimiento    DATE             NOT NULL,
-	id_usuario           INT              NOT NULL,
-	CONSTRAINT FK_tareaUsuario FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
-);
+INSERT INTO Tarea(id, nombre, descripcion, nombre_usuario, fecha_inicio, fecha_vencimiento, estatus) 
+VALUES(1, "Tarea 1", "User1", "descripcion", "22/08/2022", "23/08/2022", 0);
+INSERT INTO Tarea(id, nombre, descripcion, nombre_usuario, fecha_inicio, fecha_vencimiento, estatus) 
+VALUES(2, "Tarea 2", "User1", "descripcion_2", "22/08/2022", "23/08/2022", 0);
+INSERT INTO Tarea(id, nombre, descripcion, nombre_usuario, fecha_inicio, fecha_vencimiento, estatus) 
+VALUES(3, "Tarea 3", "User1", "descripcion_3", "22/08/2022", "23/08/2022", 0);
+
+INSERT INTO Empresa(id, nombre, id_tarea) VALUES(1, "Empresa 1", 1);
+
+INSERT INTO Usuario(id, nombre, id_tarea) VALUES(1, "Usuario 1", 1);
+
+SELECT e.nombre, t.nombre, t.estatus, u.nombre
+FROM Empresa AS e
+INNER JOIN Tarea AS t
+INNER JOIN Usuario AS u
+WHERE e.id_tarea = t.id
+AND u.id_tarea = t.id
+AND t.estatus != 1;
+
+/*
+SELECT *
+FROM Tarea
+WHERE estatus = 0;
+*/
 
 
 
